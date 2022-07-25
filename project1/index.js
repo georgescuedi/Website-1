01017b6c420702bc807d6fefa77760801b514aab
Georgescu Eduard <109967198+georgescuedi@users.noreply.github.com>
2022-07-25 17:23:21 +0300

    Add files via upload

---
+++ project1/index.js
@@ -0,0 +1,20 @@
+const age=18;
+
+if(age<18){
+console.log("You are under aged.")
+} else{
+     console.log("You are an adult.")
+}
+
+
+// const people=["Bogdan", "Mihai", "Edi", "Andreea"];
+// for(i=people.length -1 ;i>=0;i--){
+// console.log(people[i]);
+// }
+ 
+// let number=1;
+// while( number<10){
+// console.log(number);
+// number++;
+// }
+
